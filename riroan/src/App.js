381e19972baac4c65e8041b8bd4820b88cc2d634
riroan <riroan@naver.com>
2024-03-30 02:10:11 +0900

    기본 내용 작성

---
+++ riroan/src/App.js
@@ -0,0 +1,187 @@
+import React from 'react'
+
+export default function App() {
+	return (
+		<div>
+			<span>김명기</span>
+			<span> (riroan)</span>
+			<br />
+			1998. 09. 09.
+			<br />
+			Backend Developer
+			<hr />
+			<br />
+			안녕하세요
+			<br />
+			<ul>
+				<li>언제나 현재 코드를 개선할 수 있는지 고민합니다.</li>
+				<li>What보다는 How와 Why를 중요하게 생각합니다.</li>
+				<li>알고 있는 지식을 알려주고 배우며 함께 성장하는 것을 지향합니다.</li>
+			</ul>
+			<h1>WORK EXPERIENCES</h1>
+			<hr />
+			<h2>
+				카카오 Server Developer <span>2024. 03. -</span>
+			</h2>
+			<h2>
+				카카오 (인턴) Server Developer <span>2024. 01. - 2024. 03.</span>
+			</h2>
+			<h2>
+				알로카도스 Backend Developer <span>2023. 03. - 2023. 12.</span>
+			</h2>
+			<h1>PROJECTS</h1>
+			<hr />
+			<h2>
+				APAP (Abnormal Pattern Alarmer Platform) <span>2021. 06. - 2021. 12.</span>
+			</h2>
+			<span>#Python #FastAPI #Pytorch #AWS #CV #React #Typescript</span>
+			<br />
+			소프트웨어 마에스트로 12기에서 수행한 프로젝트입니다. CCTV에서 이상행동을 감지하는 프로젝트로 Pytorch를 사용한 AI모델 개발/학습/배포를 진행했습니다. 추가로 관리자를 위한 Backoffice
+			페이지도 개발했습니다.
+			<ul>
+				<li>CI/CD/CT 파이프라인 구성</li>
+				<li>Backbone Network 개선, Data augmentation, Fine tuning 학습 기법을 통해 탐지율 85% -> 93% 개선</li>
+				<li>모델 개발을 위한 SOTA 논문 정독 및 적용 </li>
+				<li>Agile 개발 방법론 사용</li>
+				<ul>
+					<li>데일리 스크럼 수행</li>
+					<li>2주단위 스프린트 4회 진행</li>
+				</ul>
+			</ul>
+			<h2>
+				MKCloud <span>2023. 01. - 2023. 05.</span>
+			</h2>
+			<span>#Typescript #React #Java #Spring #MySQL #Nginx #Oracle Cloud #Docker #Github Action</span>
+			<br />
+			개인용 스토리지를 편리하게 사용할 수 있도록 개발한 1인 프로젝트입니다. 프론트엔드, 백엔드 개발을 모두 담당했습니다.
+			<ul>
+				<li>Nginx를 사용하여 리버스 프록시 적용, 최대 전송 파일 크기 제한</li>
+				<li>Docker Compose, Github Action을 사용하여 CICD 구성 </li>
+				<li>Spring Data JPA를 사용하여 ORM 적용</li>
+			</ul>
+			<h2>
+				Kunsolved <span>2022. 04. - 2022. 10.</span>
+			</h2>
+			<span>#Typescript #React #Python #FastAPI #MySQL #AWS #Docker #Github Action</span>
+			<br />
+			건국대학교 학생이 해결하지 않은 백준 문제를 분석하고 리스트를 제공하는 1인 프로젝트입니다. 프론트엔드, 백엔드 개발을 모두 담당했습니다.
+			<ul>
+				<li>Docker Compose, Github Action을 사용하여 CICD 구성</li>
+				<li>Crontab을 사용하여 주기적인 작업 수행</li>
+				<li>ACM에서 SSL을 발급하여 Loadbalancer에 적용하여 HTTPS 통신 및 로드밸런싱</li>
+			</ul>
+			<h2>
+				Lollang <span>2022. 04.</span>
+			</h2>
+			<span>#Python</span>
+			<br />
+			만우절을 기념하여 롤에서 사용하는 채팅을 모방하여 프로그래밍하는 난해한 프로그래밍 언어를 개발했습니다.
+			<ul>
+				<li>문법 정의 및 로직 구현</li>
+				<li>롤랭 위키 작성</li>
+			</ul>
+			<h1>EDUCATION</h1>
+			<hr />
+			건국대학교 컴퓨터공학부 17학번 학사 졸업
+			<br />
+			GPA 3.75/4.50 (CSE 3.75/4.50)
+			<h1>SKILLS</h1>
+			<hr />
+			<h2>FLUENT</h2>
+			#Python #FastAPI #Django #Docker #Github Action #Slack SDK
+			<h2>SO-SO</h2>
+			#C++ #Java #Javascript #Typescript #Kubernetes #Spring #TensorFlow #React
+			<h2>OTHERS</h2>
+			#Computer Vision #Number Theory #Abstract Algebra #Algebra #Linear Algebra
+			<h1>ACHIEVEMENTS</h1>
+			<hr />
+			<h2>
+				ICPC <span>International Collegiate Programming Contest</span>
+			</h2>
+			2021, Asia Seoul Regional Contest
+			<br />
+			2018, Asia Seoul Regional Contest
+			<h2>Meta Hacker Cup</h2>
+			2023, Competed in Round 2
+			<br />
+			2022, Competed in Round 2
+			<h2>
+				UCPC <span>전국 대학생 프로그래밍 대회</span>
+			</h2>
+			2021, 41<sup>th</sup> place
+			<h2>엘리스 코드 챌린지</h2>
+			2023, 최우수상
+			<h2>현대모비스 알고리즘 경진대회</h2>
+			2023, 장려상
+			<h2>탑 프로그래머스</h2>
+			2022, 탑 프로그래머스 취득
+			<h2>쇼미더 코드</h2>
+			2022, 1회차 금손 취득
+			<h1>ALGORITHM EXPERIENCES</h1>
+			<hr />
+			<h2>
+				백준 문제 해결 수 3800<sup>+</sup>
+			</h2>
+			국내에서 가장 큰 알고리즘 문제 해결 플랫폼입니다. 현재 랭킹 65등에 위치하고 있습니다.
+			<h2>솔브드 다이아 2티어</h2>
+			백준에서 해결한 문제의 난이도에 따라 레이팅을 얻는 시스템입니다. 현재 278등에 위치하고 있고 상위 0.22%에 해당합니다.
+			<h2>솔브드 아레나 최고 레이팅 2234 (SS+)</h2>
+			백준에서 개최되는 대회 성적에 따라 레이팅을 얻는 시스템입니다. 현재 44등에 위치하고 있고 상위 1.44%에 해당합니다.
+			<h2>Codeforces 최고 레이팅 1,932 (Candidate Master)</h2>
+			전세계적으로 인기가 많은 경쟁적 알고리즘 문제 해결 플랫폼입니다. 레이팅 1,932는 상위 3.91%에 해당합니다.
+			<h2>Atcoder 최고 레이팅 1,369 (4Kyu)</h2>
+			일본에서 만들어진 경쟁적 알고리즘 문제 해결 플랫폼입니다. 레이팅 1,369는 상위 7.58%에 해당합니다.
+			<h2>KUPC Konkuk University Programming Contest</h2>
+			건국대학교에서 개최한 알고리즘 문제 해결 대회입니다.
+			<br />
+			<h3>KUPC 2022 운영, 출제 및 검수</h3>
+			<ul>
+				<li>만쥬의 식사</li>
+				<li>비숍 여행</li>
+				<li>시험자리 배정하기</li>
+				<li>즐거운 XOR</li>
+			</ul>
+			<h3>KUPC 2023 출제 및 검수</h3>
+			<ul>
+				<li>K-문자열</li>
+				<li>MEXchange</li>
+			</ul>
+			<h1>ACTIVITIES</h1>
+			<hr />
+			<h2>
+				FastAPI 오픈소스 기여 <span>2024. 01 - </span>
+			</h2>
+			FastAPI 공식문서 번역에 기여했습니다.
+			<h2>
+				SW 마에스트로 12기 <span>2021. 06. - 2021. 12.</span>
+			</h2>
+			한국정보산업연합회에서 주관하는 SW인재양성 프로그램입니다.
+			<h2>
+				육군 SW 개발병 <span>2019. 05. - 2020. 12.</span>
+			</h2>
+			SW 개발병으로 군 복무를 수행했습니다.
+			<h1>CONTACT WITH ME</h1>
+			<hr />
+			이메일: mail://riroan@naver.com
+			<br />
+			블로그: https://riroan.tistory.com
+			<br />
+			인스타그램: https://www.instagram.com/dding_gi_98
+			<br />
+			깃허브: https://github.com/riroan
+			<br />
+			백준: https://www.acmicpc.net/user/riroan
+			<br />
+			솔브드: https://solved.ac/profile/riroan
+			<br />
+			코드포스: https://codeforces.com/profile/
+			<br />
+			앳코더: https://atcoder.jp/users/riroan
+			<h1>GAMES</h1>
+			<hr />
+			리그오브레전드: 명기명기김명기#KMG, 띵기명기김명기#KR1
+			<br />
+			메이플스토리: 학다리접기
+		</div>
+	)
+}
