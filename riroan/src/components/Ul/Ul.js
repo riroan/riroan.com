fffdf54cb2ad01db8a7b2d49fbbf3b4031f5c0fc
riroan <riroan@naver.com>
2024-04-10 01:03:26 +0900

    다크모드 효과 적용

--- riroan/src/components/Ul/Ul.js
+++ riroan/src/components/Ul/Ul.js
@@ -3,7 +3,7 @@ import styles from './Ul.module.css'
 
 export default function Ul(props) {
 	const defaultStyle = { transition: 'max-height 0.3s ease 0s, opacity 0.3s ease 0s', transformOrigin: 'center top' }
-	const visibleStyle = { maxHeight: '1000px', opacity: 1, pointerEvents: 'all', ...defaultStyle }
+	const visibleStyle = { maxHeight: '500px', opacity: 1, pointerEvents: 'all', ...defaultStyle }
 	const invisibleStyle = { maxHeight: '0px', opacity: 0, pointerEvents: 'none', ...defaultStyle }
 	return (
 		<ul style={props.display ? visibleStyle : invisibleStyle} className={styles.container}>
